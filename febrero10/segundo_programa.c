774bdf8936432d3dd147d38b508fa774d6c30cb4
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-10 11:57:08 -0600

    hice el segundo programa

---
+++ febrero10/segundo_programa.c
@@ -0,0 +1,9 @@
+/*creado el 10 de febrero del 2025 por abisay*/
+#include<stdio.h>
+int main()
+{
+printf("arriba la MAQUINAAAAAAA \n");
+printf("GOOOOOL del cruz azul \n");
+}
+/*termine el programa*/
+
