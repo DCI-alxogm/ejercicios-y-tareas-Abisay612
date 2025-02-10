b9724993c93445feb0c4e28d3ac97760f715ef2f
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-10 11:35:55 -0600

    realize mi primer programa xd

---
+++ febrero10/primer_programa.c
@@ -0,0 +1,8 @@
+/*creado el 10 de febrero del 2025 por abisay*/
+#include<stdio.h>
+int main()
+{
+printf("hola abisay xd \n");
+printf("este es el primer programa del curso \n");
+}
+/*termine de escribir el programa*/
