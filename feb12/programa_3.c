396e99da846e745bf84ee61a394a01ab199f7c46
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-12 12:03:40 -0600

    programa 3

---
+++ feb12/programa_3.c
@@ -0,0 +1,16 @@
+/*febrero 12, 2015, abisay programa 3, variables*/
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+char nombre[10];
+int edad;
+float temperatura;
+printf("¿cual es tu nombre? \n");
+scanf("%s",nombre);
+printf("hola,  %s este es mi 3er programa del curso \n", nombre);
+printf("introduce tu edad \n");
+scanf("%i", &edad);
+printf("¿que temperatura marco el termometro? \n");
+scanf("%", &temperatura);
+
+}
