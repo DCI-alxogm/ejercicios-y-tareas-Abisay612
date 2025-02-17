e06d187ccda2b00b6b301401d2839d0bb2328954
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-17 12:11:28 -0600

    operaciones float

---
+++ feb17/operaciones_float.c
@@ -0,0 +1,22 @@
+/*operaciones float feb 17*/
+
+#include <stdio.h>
+int main(){
+float a,b,c,d;
+float e1,e2,e3,e4;
+a=3.45;
+b=4.2;
+c=5.4;
+d=7.5;
+e1=(a+b)*c/d;
+e2=((a+b)*c)/d;
+e3=(a+b)*(c/d);
+e4=a+b*c/d;
+printf("e=(a+b)*c/d=%f\n",e1);
+printf("e=((a+b)*c)/d=%f\n",e2);
+printf("e=(a+b)*(c7d=%f\n",e3);
+printf("e=a+b*c/d=%f\n",e4);
+printf("%f\n",(a+b)*c/d);
+printf("%f\n",e1);
+
+}
