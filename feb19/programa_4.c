deffce70bae00079812e4d655f8a59f3f930fcb4
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-19 11:26:15 -0600

    adgoridmo de grados c a k

---
+++ feb19/programa_4.c
@@ -0,0 +1,13 @@
+/*19 de febrearo del 2025*/
+/*convertir de celcius a kelvin*/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+float c,k;
+printf("temperatura en grados \n");
+scanf("%f", &c);
+k=c+273.15;
+printf(" la temperatura en kelvin es : %f \n",k);
+
+}
