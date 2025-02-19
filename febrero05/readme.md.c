96e37c1fac742d73862c0c37618d846db951be3f
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-19 10:36:37 -0600

    explique como atualizar github

---
+++ febrero05/readme.md.c
@@ -0,0 +1,2 @@
+primero actualizo el github con gitpull luego para añadir el archivo pongo git add
+luego git commit y  para guardarlo el hithub pongo gitpush pongo mi nombre de usario y contraseña del token
