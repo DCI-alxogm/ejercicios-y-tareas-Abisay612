73befbafa166720255efacb493dddd10f63dff6b
abisay612 <jea.gutierrezlosda@ugto.mx>
2025-02-19 11:55:25 -0600

    empeze el adgoridmo de corndenadas

---
+++ feb19/cordenadas.c
@@ -0,0 +1,10 @@
+/*febrero 19 trasformacion de cordenadas*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+float r,A,T,x.y.z,r2,tanT;
+printf("valor de la equis \n")
+scantf("%f", &x);
